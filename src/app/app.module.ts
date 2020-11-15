e2dd6f84fdc461248b7467a4926876769e86a2cc
Rahul Kumar <rahulkumar151995@gmail.com>
2020-11-15 19:59:11 +0530

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AuthenticationComponent } from './authentication/authentication.compone
 import { ChatRoomComponent } from './chat-room/chat-room.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'https://chat-room-gbu.herokuapp.com', options: {} };
 
 @NgModule({
   declarations: [
